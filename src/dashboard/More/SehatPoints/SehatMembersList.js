11a90cd23c2bbd0637c4f7242b4f4bfd763e29ff
yogeshShopno <yogesh.shopno@gmail.com>
2026-01-12 15:03:06 +0530

    remove console

--- src/dashboard/More/SehatPoints/SehatMembersList.js
+++ src/dashboard/More/SehatPoints/SehatMembersList.js
@@ -111,7 +111,6 @@ const SehatMembersList = () => {
             })
                 .then((response) => {
                     setMemberDetails(response.data.data);
-                    console.log(response.data.data)
                 });
         } catch (error) {
             console.error("API error:", error?.response?.status);

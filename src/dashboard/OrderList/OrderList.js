921467d10904c9e58c1fcf5f9794beeb9e655171
Harshit Kapadia <harshitkapadia563@gmail.com>
2026-02-20 14:53:52 +0530

    pending order

--- src/dashboard/OrderList/OrderList.js
+++ src/dashboard/OrderList/OrderList.js
@@ -888,7 +888,7 @@ toast.success(response.data.meassage);
               </DialogContent>
             </Dialog>
           {/*<=============================================== order dialog  ==============================================> */}
-            <Dialog className=" custom-dialog " open={openAddPopUpPlaceOrder}>
+            <Dialog className=" custom-dialog " open={openAddPopUpPlaceOrder} style ={{ width: '100%' }}>
               <DialogTitle
                 id="alert-dialog-title"
                 style={{ fontWeight: 700 }}
